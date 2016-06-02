b1c8c41501566bd43cb0fc92da2d3554dd4efc25
Ben <Ben>
2016-06-02 21:39:55 +0200

    Added Style for Calc

--- calc/ccm.calc.js
+++ calc/ccm.calc.js
@@ -3,15 +3,27 @@ ccm.component({
     /*-------------------------------------------- public component members --------------------------------------------*/
 
     name: 'calc',
+    
+    config: {
+        html:  [ ccm.store, { local: 'templates.json' } ],
+        style: [ ccm.load, 'style.css' ],
+
+
+    },
 
     /*-------------------------------------------- public component classes --------------------------------------------*/
 
     Instance: function () {
 
+        var self = this;
+
+
         /*------------------------------------------- public instance methods --------------------------------------------*/
         this.render = function (callback) {
 
             var element = ccm.helper.element(this);
+
+            element.html( ccm.helper.html( self.html.get( 'main' ) ) );
 
 
             if (callback) callback();
